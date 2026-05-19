c0838f295d8bc1ce2a5f990bd319f56a4dcaae01
flaviomassayoshi <95700923+flaviomassayoshi@users.noreply.github.com>
2026-05-19 00:34:02 -0300

    release(staging): rebuilt [] at 2026-05-19T03:33:57.728Z

--- canonical/viewers/planet-hall/main.ts
+++ canonical/viewers/planet-hall/main.ts
@@ -5,6 +5,7 @@ import en from './i18n/en.json'
 import pt from './i18n/pt.json'
 
 const i18n = createI18n({
+  legacy: false,
   locale: navigator.language?.split('-')[0] || 'en',
   fallbackLocale: 'en',
   messages: { en, pt },
